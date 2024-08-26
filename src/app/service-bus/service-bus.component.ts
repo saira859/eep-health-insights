c5e0f6308b21b15e2a1a59122ce520fa0a51bd2a
Saira <sairarafi859@gmail.com>
2024-08-26 14:20:12 +0530

    changes

---
+++ src/app/service-bus/service-bus.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-bus',
+  standalone: true,
+  imports: [],
+  templateUrl: './service-bus.component.html',
+  styleUrl: './service-bus.component.css'
+})
+export class ServiceBusComponent {
+
+}
